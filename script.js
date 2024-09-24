f46b2d8963ed9bccb139826cee5ceeb864c89d68
NAYAMAAAAT <atefmdnayamatullah@gmail.com>
2024-09-24 19:47:20 +0600

    9th commit

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ document.getElementById("blog-btn").addEventListener("click", function () {
   }
 
 
-
   document.getElementById("history-btn").addEventListener("click", function () {
     document.getElementById("history-section").classList.remove("hidden");
     document.getElementById("card-section").classList.add("hidden");
@@ -78,4 +77,90 @@ document.getElementById("blog-btn").addEventListener("click", function () {
   document.getElementById("history-section").appendChild(history);
   document.getElementById("my_modal").showModal();
   document.getElementById("amount-input1").value = "";
+  });
+
+
+
+
+
+
+
+  document.getElementById("donate-btn2").addEventListener("click", function () {
+    const amount = inputFieldValueById("amount-input2");
+    const balance = getInnerTextById("balance");
+    const donatedMoney = getInnerTextById("donate-counter2");
+    if (isNaN(amount)) {
+      alert("Invalid Amount");
+      return;
+    }
+    if (amount <= 0 || balance < amount) {
+      alert("Invalid Amount");
+      return;
+    }
+  
+    const newBalance = balance - amount;
+    document.getElementById("balance").innerText = newBalance;
+  
+    const NewDonatedMoney = amount + donatedMoney;
+    document.getElementById("donate-counter2").innerText = NewDonatedMoney;
+  
+    const history = document.createElement("div");
+    history.innerHTML = `
+              <div class="border-2 border-[#1111111A] shadow-xl rounded-2xl p-5 mb-6">
+              <h3 class="text-lg font-bold">
+                ${amount} Taka is Donated for Flood at Feni, Bangladesh
+              </h3>
+              <p class="text-lg font-normal">
+                Date: ${new Date()}
+              </p>
+            </div>
+      `;
+  
+    document.getElementById("history-section").appendChild(history);
+    document.getElementById("my_modal").showModal();
+    document.getElementById("amount-input2").value = "";
+  });
+
+
+
+
+
+
+
+
+
+  document.getElementById("donate-btn3").addEventListener("click", function () {
+    const amount = inputFieldValueById("amount-input3");
+    const balance = getInnerTextById("balance");
+    const donatedMoney = getInnerTextById("donate-counter3");
+    if (isNaN(amount)) {
+      alert("Invalid Amount");
+      return;
+    }
+    if (amount <= 0 || balance < amount) {
+      alert("Invalid Amount");
+      return;
+    }
+  
+    const newBalance = balance - amount;
+    document.getElementById("balance").innerText = newBalance;
+  
+    const NewDonatedMoney = amount + donatedMoney;
+    document.getElementById("donate-counter3").innerText = NewDonatedMoney;
+  
+    const history = document.createElement("div");
+    history.innerHTML = `
+              <div class="border-2 border-[#1111111A] shadow-xl rounded-2xl p-5 mb-6">
+              <h3 class="text-lg font-bold">
+                ${amount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh
+              </h3>
+              <p class="text-lg font-normal">
+                Date: ${new Date()}
+              </p>
+            </div>
+      `;
+  
+    document.getElementById("history-section").appendChild(history);
+    document.getElementById("my_modal").showModal();
+    document.getElementById("amount-input3").value = "";
   });
